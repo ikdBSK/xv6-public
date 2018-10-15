4cae15a1c25f4f3fab0820073b0322d4f07a9472
ikdBSK <3p.ahirunosora@gmail.com>
2018-10-15 17:34:41 +0900

    syssoft

--- Makefile
+++ Makefile
@@ -181,6 +181,8 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
+	_date\
+	_sleepsec\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
--- user.h
+++ user.h
@@ -37,3 +37,5 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
+int getdate(struct rtcdate *);
+int sleep_sec(int sec);
